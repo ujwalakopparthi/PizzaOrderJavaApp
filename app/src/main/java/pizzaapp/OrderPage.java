c14172b025e8e984cdf8c9b46ec773e59fe80aee
ujwalakopparthi <ujwalakopparthi1@gmail.com>
2025-02-18 18:27:08 -0500

    Add files via upload

---
+++ app/src/main/java/pizzaapp/OrderPage.java
@@ -0,0 +1,12 @@
+package com.SAURABH.pizza;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class OrderPage extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.order_page);
+    }
+}
